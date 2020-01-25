8e66cd2bcd9142d44a50c52be540f940e5fd4a99
Adam Zawierucha <zawiedev@gmail.com>
2020-01-24 22:28:45 -0600

    split func

--- data.py
+++ data.py
@@ -18,3 +18,10 @@ def get():
                 y.append(elements.pop()) #add divorce stat to y list
                 x.append(elements)  #add input matrix
     return x,y
+
+def split(lst,num_train):
+    train_lst = []
+    for i in range(num_train):
+        r = random.randint(0,len(lst)-1)
+        train_lst.append(lst.pop(r))
+    return  train_lst,lst
