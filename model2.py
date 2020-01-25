7621dbc9733f63b16d9b6b73e1e63488c96c5b8f
Adam Zawierucha <zawiedev@gmail.com>
2020-01-25 13:18:49 -0600

    actual run

--- model2.py
+++ model2.py
@@ -6,9 +6,18 @@ from sklearn.feature_selection import RFE
 import data
 import time
 import copy
+import csv
 import matplotlib.pyplot as plt
 
+NUM_TRIALS = 1000
+
 inputs, labels = data.get_shuffled()
+headers = list(range(1,55))
+headers_history = []
+
+rank_history = {}
+for i in range(1, 55):
+    rank_history[i] = 0
 
 def getColumns():
     return len(inputs[0])
@@ -31,8 +40,11 @@ def getWorstColumn(X,y):
             return i
 
 def removeColumn(X,col):
+    q_num = headers.pop(col)
+    rank_history[q_num] += len(headers)
     for r in range(len(X)):
         X[r].pop(col)
+    return q_num
 
 def trial():
     results = []
@@ -45,12 +57,15 @@ def trial():
         point = (getColumns(),accuracy)
         x_axis.append(point[0])
         y_axis.append(point[1])
+        headers_history.append(headers[:])
+        #print(headers,len(headers))
         #print(point)
         results.append(point)
+    rank_history[headers[0]] += 1
     return results
 
 family = dict()
-for t in range(100):
+for t in range(NUM_TRIALS):
     print("Trial",t)
     results = trial()
     for point in results:
@@ -59,14 +74,40 @@ for t in range(100):
         if x not in family:
             family[x] = []
         family[x].append(y)
-    inputs, labels = data.get_shuffled() #restore data
+    #regenerate
+    inputs, labels = data.get_shuffled()
+    headers = list(range(1,55))
 
+
+## nums qs
 x_axis = []
 y_axis = []
 for key,values in family.items():
     x_axis.append(key)
     average = sum(values)/len(values)
     y_axis.append(average)
+
+with open('numQs_vs_accuracy.csv', mode='w') as results_file:
+    writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for i in range(len(x_axis)):
+        x = x_axis[i]
+        y = y_axis[i]*100
+        writer.writerow([x,y])
+
+
+## ranking
+ranking = []
+for i in range(1, 55):
+    ranking.append((rank_history[i]/NUM_TRIALS, i))
+ranking.sort()
+
+with open('average_rank_of_qs.csv', mode='w') as results_file:
+    writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for el in ranking:
+        print(el)
+        x = el[1]
+        y = el[0]
+        writer.writerow([x,y])
 
 plt.plot(x_axis,y_axis)
 plt.show()
