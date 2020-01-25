9453622f2024fa3aec72b330b06a54672394121e
Adam Zawierucha <zawiedev@gmail.com>
2020-01-25 10:49:33 -0600

    updates

--- model.py
+++ model.py
@@ -5,32 +5,47 @@ import time
 import copy
 
 removed = []
+inputs, labels = data.get_shuffled()
+num_rows = len(inputs)
 
-inputs, categories = data.get_shuffled()
+def trainAndTest(X,y):
+    clf = svm.SVC()
+    cv_results = cross_validate(clf, X, y, cv=2)
+    results = data.assess_results(cv_results)
+    average_accuracy = mean(results)
+    return average_accuracy
+
+def getColumns():
+    return len(inputs[0])
+
+def removeColumn(X,col):
+    rem_col = []
+    for r in len(X):
+        rem_col.append(X[r].pop(col))
+    return rem_col
 
 #TODO: switch to True once we actually want to run it
-while len(data) > 0:
+
+while getColumns() > 0:
+    num_columns = getColumns()
     lowest_impact_idx = 0
     lowest_impact_cv_results = None
-    for idx in range(categories):
+    for c in num_columns:
+        #copy and remove column
         temp_inputs = copy.deepcopy(inputs)
-        temp_categories = copy.deepcopy(categories)
-        temp_inputs.pop(idx)
-        temp_categories.pop(idx)
-
-        clf = svm.SVC()
-        cv_results = cross_validate(clf, temp_inputs, temp_categories, cv=2)
-        results = data.assess_results(cv_results)
-        if lowest_impact_cv_results is None or results < lowest_impact_cv_results:
-            lowest_impact_idx = idx
-            lowest_impact_cv_results = results
+        removeColumn(temp_inputs,c)
+        #train & test
+        average_accuracy = trainAndTest(temp_inputs,labels)
+        if (lowest_impact_cv_results is None)or (average_accuracy < lowest_impact_cv_results):
+            lowest_impact_idx = c
+            lowest_impact_cv_results = average_accuracy
 
     #Put the break if accuracy gets too low here
 
     #Stores info about the removed row
     #(data in row, category of row, results of removal)
-    lowest_val_row = (data.pop(lowest_impact_idx), categories(lowest_impact_idx), lowest_impact_cv_results)
-    removed.append(lowest_val_row)
+    lowest_val_col = (removeColumn(inputs), lowest_impact_idx, lowest_impact_cv_results, len(inputs[0]))
+    removed.append(lowest_val_col)
 
 inputs, categories = data.get_shuffled()
 clf = svm.SVC()
